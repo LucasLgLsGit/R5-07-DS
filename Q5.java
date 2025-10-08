c8adbcdbb360913a9e1a2753ecb93353014e5cf4
Lucas LANGLOIS <166138110+LucasLgLsGit@users.noreply.github.com>
2025-10-08 16:03:34 +0200

    inverser l’affichage du triangle #1

--- Q5.java
+++ Q5.java
@@ -1,11 +1,11 @@
 public class Q5 {
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.out.println("Donnez une variable ");
+            System.out.println("Donnez une variable N :");
             return;
         }
         int N = Integer.parseInt(args[0]);
-        for (int i = 1; i <= N; i++) {
+        for (int i = N; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
